d1c142207ef766489e43c1dc37964772e3583bd6
Amri05 <bahinipatiamrita79@gmail.com>
2026-04-30 23:40:11 +0530

    Create SubtractionMatrix.java

---
+++ SubtractionMatrix.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int r = sc.nextInt(), c = sc.nextInt();
+
+        int[][] A = new int[r][c];
+        int[][] B = new int[r][c];
+
+        for (int i = 0; i < r; i++)
+            for (int j = 0; j < c; j++)
+                A[i][j] = sc.nextInt();
+
+        for (int i = 0; i < r; i++)
+            for (int j = 0; j < c; j++)
+                B[i][j] = sc.nextInt();
+
+        System.out.println("Result:");
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++)
+                System.out.print((A[i][j] - B[i][j]) + " ");
+            System.out.println();
+        }
+    }
+}
